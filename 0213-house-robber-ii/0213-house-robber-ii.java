1a1ad36f3e409f7b9843b323253617b1fbcb287a
Lakshay Pratap Singh <rajputlakshay0014@gmail.com>
2026-06-24 23:18:40 +0530

    Time: 0 ms (100%), Space: 42.8 MB (59.58%) - LeetHub

---
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -0,0 +1,35 @@
+class Solution {
+
+    public int rob(int[] nums) {
+
+        int n = nums.length;
+
+        if(n == 1) {
+            return nums[0];
+        }
+
+        return Math.max(
+            robHouse(nums, 0, n - 2),
+            robHouse(nums, 1, n - 1)
+        );
+    }
+
+    private int robHouse(int[] nums,
+                         int start,
+                         int end) {
+
+        int prev1 = 0;
+        int prev2 = 0;
+
+        for(int i = start; i <= end; i++) {
+
+            int curr =
+                Math.max(prev1, prev2 + nums[i]);
+
+            prev2 = prev1;
+            prev1 = curr;
+        }
+
+        return prev1;
+    }
+}
