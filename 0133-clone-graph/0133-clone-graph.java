a00873fa35e213d002a50483d8bc5d34e73214bc
Lakshay Pratap Singh <rajputlakshay0014@gmail.com>
2026-06-26 23:49:45 +0530

    Time: 25 ms (78.48%), Space: 44.4 MB (39.61%) - LeetHub

---
+++ 0133-clone-graph/0133-clone-graph.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class Solution {
+
+    private HashMap<Node, Node> map = new HashMap<>();
+
+    public Node cloneGraph(Node node) {
+
+        if (node == null)
+            return null;
+
+        if (map.containsKey(node))
+            return map.get(node);
+
+        Node clone = new Node(node.val);
+
+        map.put(node, clone);
+
+        for (Node nei : node.neighbors) {
+            clone.neighbors.add(cloneGraph(nei));
+        }
+
+        return clone;
+    }
+}
