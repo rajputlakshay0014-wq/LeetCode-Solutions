7f2aa3959a83acb60a887aa79e875c8015e362c9
Lakshay Pratap Singh <rajputlakshay0014@gmail.com>
2026-07-06 22:55:01 +0530

    Time: 11 ms (65.53%), Space: 46.8 MB (72.18%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
@@ -1,47 +1,29 @@
-import java.util.*;
-
 class Solution {
-
     public List<Integer> findAnagrams(String s, String p) {
-
         List<Integer> result = new ArrayList<>();
-
-        // Edge case
-        if (s.length() < p.length()) {
+        if(s.length() < p.length()){
             return result;
         }
-
         int[] pCount = new int[26];
         int[] sCount = new int[26];
-
-        // Fill frequency arrays
-        for (int i = 0; i < p.length(); i++) {
-
+        for(int i=0;i<p.length();i++){
             pCount[p.charAt(i) - 'a']++;
-
             sCount[s.charAt(i) - 'a']++;
         }
-
-        // Check first window
-        if (Arrays.equals(pCount, sCount)) {
+        if(Arrays.equals(pCount , sCount)){
             result.add(0);
         }
 
-        // Sliding window
-        for (int i = p.length(); i < s.length(); i++) {
-
-            // Add new character
+        for(int i = p.length();i<s.length();i++){
             sCount[s.charAt(i) - 'a']++;
 
-            // Remove old character
-            sCount[s.charAt(i - p.length()) - 'a']--;
-
-            // Compare arrays
-            if (Arrays.equals(pCount, sCount)) {
-                result.add(i - p.length() + 1);
+            sCount[s.charAt(i-p.length()) - 'a']--;
+            if(Arrays.equals(pCount,sCount)){
+                result.add(i-p.length() + 1);
             }
         }
-
         return result;
+
+        
     }
 }
