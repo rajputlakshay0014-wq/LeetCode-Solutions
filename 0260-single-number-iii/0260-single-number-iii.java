72761d130354d922152124cdecd94d5e9c4fbd78
Lakshay Pratap Singh <rajputlakshay0014@gmail.com>
2026-08-04 20:45:24 +0530

    Time: 1 ms (100%), Space: 47.8 MB (78.34%) - LeetHub

---
+++ 0260-single-number-iii/0260-single-number-iii.java
@@ -0,0 +1,26 @@
+class Solution {
+
+    public int[] singleNumber(int[] nums) {
+
+        int xor = 0;
+
+        for (int num : nums) {
+            xor ^= num;
+        }
+
+        int diff = xor & (-xor);
+
+        int a = 0;
+        int b = 0;
+
+        for (int num : nums) {
+
+            if ((num & diff) == 0)
+                a ^= num;
+            else
+                b ^= num;
+        }
+
+        return new int[]{a, b};
+    }
+}
